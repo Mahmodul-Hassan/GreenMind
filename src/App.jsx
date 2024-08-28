eea11c5c929e4ab0720a204df413e56cdef5ea8f
Mahmodul-Hassan <mahmudmahmud9200@gmail.com>
2024-08-28 13:12:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import './assets/css/tailwind.css';
+// import './index.css'
+function App() { 
+
+  return (
+    <>
+      <div className="bg-red-400">Hello World</div>
+      <h2>hi</h2>
+
+    </>
+  )
+}
+
+export default App
+
+
